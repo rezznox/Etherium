ecbee45c6c9998e2e103387a4382a5fdb78bd04f
Armando José Sáenz Mariño <yo_rulez@hotmail.com>
2013-09-30 17:54:11 -0500

    commit con todo!

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+	// Use this for initialization
+	private float mousePosX; 
+    private float mousePosY;
+	private Ray rayH;
+	private RaycastHit hit;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+			rayH = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if(Physics.Raycast(rayH, out hit, 50))
+			{
+				mousePosX = hit.point.x; 
+		    	mousePosY = hit.point.y;
+			}
+		}
+		transform.position = Vector3.Lerp(transform.position, new Vector3(mousePosX, mousePosY, 0), Time.deltaTime * 4);
+
+	}
+}
---
+++ Assets/MovimientoCamara.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovimientoCamara : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	 
+	}
+ 
+	// Update is called once per frame
+	void Update () {
+ 
+    float mousePosX = Input.mousePosition.x; 
+    float mousePosY = Input.mousePosition.y; 
+    int scrollDistance = 5; 
+    float scrollSpeed = 45;
+ 
+    if (mousePosX < scrollDistance) 
+         { 
+          transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
+         } 
+ 
+    if (mousePosX >= Screen.width - scrollDistance) 
+         { 
+          transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
+         }
+ 
+    if (mousePosY < scrollDistance) 
+         { 
+          transform.Translate(new Vector3(0,1,0.6f) * -scrollSpeed * Time.deltaTime); 
+         } 
+ 
+    if (mousePosY >= Screen.height - scrollDistance) 
+         { 
+          transform.Translate(new Vector3(0,1,0.6f) * scrollSpeed * Time.deltaTime); 
+         }
+    }
+
+}
+
