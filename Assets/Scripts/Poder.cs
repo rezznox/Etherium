e2dd4e211358e731004399544c9aa698f340343f
Sebastian Gil Parga <s.gilparga@gmail.com>
2013-10-29 21:20:42 -0500

    La colision esta mas manejable

--- Assets/Scripts/Poder.cs
+++ Assets/Scripts/Poder.cs
@@ -31,9 +31,59 @@ public class Poder: MonoBehaviour{
 		GameObject objetivo = collision.gameObject;
 		Vida vidaObjetivo = (Vida)objetivo.GetComponent(typeof(Vida));
 		if (objetivo.CompareTag ("Enemigo")){
-			objetivo.rigidbody.AddForce(transform.forward*fuerza);
+			//objetivo.rigidbody.AddForce(transform.forward*fuerza);
+		
+			Vector3 empuje = transform.forward;
+			Vector3 actual = transform.position;
+			Debug.Log(empuje);
+			float addX = 0.0f;
+			float addZ = 0.0f;
+			
+			if(empuje.x <0.0f)
+				addX-=fuerza/2;
+			if(empuje.x <= -1.0f)
+				addX-=fuerza/2;
+			if(empuje.z < 0.0f)
+				addZ-=fuerza/2;
+			if(empuje.z <= -1.0f)
+				addZ-=fuerza/2;
+			
+			
+			if(empuje.x >0.0f)
+				addX+=fuerza/2;
+			if(empuje.x >= 1.0f)
+				addX+=fuerza/2;
+			if(empuje.z > 0.0f)
+				addZ+=fuerza/2;
+			if(empuje.z >= 1.0f)
+				addZ+=fuerza/2;
+			
+			Debug.Log("X: " + addX + "Z: " + addZ);
+			Vector3 target = new Vector3(actual.x + addX, actual.y,actual.z + addZ);
+			iTween.MoveTo(objetivo,iTween.Hash("position",target,"speed",fuerza));
+			/*if(empuje.x < 0.0f ){
+				Debug.Log("X -1");
+				Vector3 target = new Vector3(actual.x - fuerza, actual.y,actual.z);
+				iTween.MoveTo(objetivo,iTween.Hash("position",target,"speed",fuerza));
+			}
+			else if(empuje.z < 0.0f ){
+				Debug.Log("Z -1");
+				Vector3 target = new Vector3(actual.x, actual.y,actual.z - fuerza);
+				iTween.MoveTo(objetivo,iTween.Hash("position",target,"speed",fuerza));
+			}
+			else if(empuje.x == 1 ){
+				Debug.Log("X 1");
+				Vector3 target = new Vector3(actual.x + fuerza, actual.y,actual.z);
+				iTween.MoveTo(objetivo,iTween.Hash("position",target,"speed",fuerza));
+			}
+			else if(empuje.z >= 1 ){
+				Debug.Log("Z 1");
+				Vector3 target = new Vector3(actual.x, actual.y,actual.z + fuerza);
+				iTween.MoveTo(objetivo,iTween.Hash("position",target,"speed",fuerza));
+			}*/
+			
 			vidaObjetivo.hayDanio(dano);
-			this.renderer.enabled = false;
+			Destroy(this.gameObject);
 		}
 	}
 	
